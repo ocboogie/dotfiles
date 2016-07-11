3e7e3ed73d469eb89657da3bc0a6d16d92af119a
boogie <boogie@mikulec.com>
2016-07-10 21:51:06 -0700

    Added emacs plugins

--- both/emacs.d/elpa/impatient-mode-20150501.247/impatient-mode-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; impatient-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "impatient-mode" "impatient-mode.el" (22402
-;;;;;;  20766 385097 971000))
-;;; Generated autoloads from impatient-mode.el
-
-(autoload 'impatient-mode "impatient-mode" "\
-Serves the buffer live over HTTP.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("impatient-mode-pkg.el") (22402 20766
-;;;;;;  478635 828000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; impatient-mode-autoloads.el ends here
